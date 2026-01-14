8c1650169c17d18f010c3004a5f1b96f07efe9fc
Teddy Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2026-01-14 19:45:17 +0800

    Bump version: 4.4.5 → 4.5.0

--- questdb_rest/__init__.py
+++ questdb_rest/__init__.py
@@ -10,7 +10,7 @@ from questdb_rest.utils import _qdb_exec_result_dict_extract_field
 # consts
 # --------------------
 
-__version__ = "4.4.5"
+__version__ = "4.5.0"
 CLI_EPILOG = """This CLI can also be used as a Python library.
 
 Links:
